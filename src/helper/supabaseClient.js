098b94aee7a62786698f19dea7bb5e14428be6f2
mark162657 <mark162657@Elon-Mac.local>
2025-10-07 01:53:17 +1100

    change .env

--- src/helper/supabaseClient.js
+++ src/helper/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Access the environment variables defined in .env.local
+// Access the environment variables defined in .env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
